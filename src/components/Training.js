c7dddf9eb5cc1f04ee1e5acaa0a45e472e8813ac
estefanychamorrotroncoso-art <estefany.chamorro.troncoso@gmail.com>
2025-08-19 19:50:39 -0400

    Create Training.js

---
+++ src/components/Training.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Training() {
+  return (
+    <div>
+      <h2>Entrenamientos</h2>
+      <p>Ejercicios sugeridos para hoy:</p>
+      <ul>
+        <li>Sentadillas asistidas x 12</li>
+        <li>Flexiones inclinadas x 10</li>
+        <li>Plancha 30s</li>
+      </ul>
+    </div>
+  );
+}
